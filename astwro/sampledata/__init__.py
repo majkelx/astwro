c87ba2552c51d964c535fb4a98ee5379c2fa103d
majkelx <majkelx.github@2007.gfdgfdg.com>
2016-10-19 17:14:35 +0200

    smample dao files into smapledata

--- astwro/sampledata/__init__.py
+++ astwro/sampledata/__init__.py
@@ -1,4 +1,28 @@
 
 def fits_image():
+    return __sampledata('NGC6871.fits')
+
+
+def coo_file():
+    return __sampledata('NGC6871.coo')
+
+
+def lst_file():
+    return __sampledata('NGC6871.lst')
+
+
+def ap_file():
+    return __sampledata('NGC6871.ap')
+
+
+def psf_file():
+    return __sampledata('NGC6871.psf')
+
+
+def als_file():
+    return __sampledata('NGC6871.als')
+
+
+def __sampledata(filename):
     import os
-    return os.path.join(os.path.abspath(os.path.dirname(__file__)), 'NGC6871.fits')
+    return os.path.join(os.path.abspath(os.path.dirname(__file__)), filename)
